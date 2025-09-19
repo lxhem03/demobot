aae97ad5c7fbd81cac43012656e5ef3dadd75e1a
Levhcwm21. <alexags1983@gmail.com>
2025-09-19 13:39:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os, time, re
+import os
+from typing import List
 
 id_pattern = re.compile(r'^.\d+$') 
 
@@ -8,9 +10,8 @@ class Config(object):
     API_ID    = os.environ.get("API_ID", "23898744")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "0b13c810c80b548604650cbe3c3db0c3") # ⚠️ Required
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6912498692:AAH0lO6ul6FdyrOG0Nh6uzKsImhFwZrGZWI") # ⚠️ Required
-    FORCE_SUB = os.environ.get('FORCE_SUB', 'Rohesh_Bots') # ⚠️ Required
-    AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
-    FORCE_SUB) else None
+    IS_FSUB = bool(os.environ.get("FSUB", True)) # Set "True" For Enable Force Subscribe
+    AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100xxxxxxxxx -100xxxxxxx").split())) # Add Multiple channel id
    
     # database config
     DB_URL  = os.environ.get("DB_URL", "mongodb+srv://filmyrohesh51:19SmDYqC1N5DqLkD@cluster0.jogzc68.mongodb.net/?retryWrites=true&w=majority")  # ⚠️ Required
