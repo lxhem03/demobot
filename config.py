a6c481dec0a57c76389bb528fc99590b2cf299e6
Levhcwm21. <alexags1983@gmail.com>
2025-09-19 13:50:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,21 +7,23 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "23898744")  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "0b13c810c80b548604650cbe3c3db0c3") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6912498692:AAH0lO6ul6FdyrOG0Nh6uzKsImhFwZrGZWI") # ⚠️ Required
+    API_ID    = os.environ.get("API_ID", "")  # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
     IS_FSUB = bool(os.environ.get("FSUB", True)) # Set "True" For Enable Force Subscribe
-    AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-100xxxxxxxxx -100xxxxxxx").split())) # Add Multiple channel id
+    AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "").split())) # Add Multiple channel id
    
     # database config
-    DB_URL  = os.environ.get("DB_URL", "mongodb+srv://filmyrohesh51:19SmDYqC1N5DqLkD@cluster0.jogzc68.mongodb.net/?retryWrites=true&w=majority")  # ⚠️ Required
-    DB_NAME  = os.environ.get("DB_NAME","SnowEncoderBot") 
+    DB_URL  = os.environ.get("DB_URL", "")  # ⚠️ Required
+    DB_NAME  = os.environ.get("DB_NAME","") 
 
     # Other Configs 
-    ADMIN = int(os.environ.get("ADMIN", "5698613889")) # ⚠️ Required
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1002077680328')) # ⚠️ Required
-    BOT_UPTIME = BOT_UPTIME  = time.time()
-    START_PIC = os.environ.get("START_PIC", "https://graph.org/file/15e82d7e665eccc8bd9c5.jpg")
+    ADMIN = int(os.environ.get("ADMIN", "")) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '')) # ⚠️ Required
+    DUMP_CHANNEL = int(os.environ.get('DUMP_CHANNEL', ''))
+    
+    BOT_UPTIME = time.time()
+    START_PIC = os.environ.get("START_PIC", "")
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
